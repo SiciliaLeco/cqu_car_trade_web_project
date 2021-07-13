89d037fa71d3c2f19807225ad98ba9028d0cf406
SiciliaLee <xslialee070@outlook.com>
2021-07-13 10:06:42 +0800

    back-end init

---
+++ src/main/java/com/example/car_trade/CarTradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarTradeApplication.class, args);
+    }
+
+}
