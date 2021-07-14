4312ad8e9d8123f02acd65759286e9c7d8dee2e9
Rayquaza <marxoffice@foxmail.com>
2021-07-15 01:03:45 +0800

    Rebuild ui project with springboot

---
+++ ui/src/main/java/com/example/teststatic/TeststaticApplication.java
@@ -0,0 +1,13 @@
+package com.example.teststatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeststaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeststaticApplication.class, args);
+    }
+
+}
