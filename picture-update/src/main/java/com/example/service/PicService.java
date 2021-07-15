8adc0818c76b549e13df0796f851cb929581efe1
MinLee810 <46177805+MinLee810@users.noreply.github.com>
2021-07-15 20:00:34 +0800

    Delete picture-update/src/main/java/com/example/service directory

--- picture-update/src/main/java/com/example/service/PicService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface PicService {
-    String upload(MultipartFile file) throws IOException;
-}
