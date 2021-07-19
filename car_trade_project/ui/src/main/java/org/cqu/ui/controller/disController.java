eb01688273adf4d2d3f760946eeddb0c93c4c99a
Sylas <49541288+ShiningChuang@users.noreply.github.com>
2021-07-19 11:55:51 +0800

    实现buyer用户中心页面修改信息的前端

--- car_trade_project/ui/src/main/java/org/cqu/ui/controller/disController.java
+++ car_trade_project/ui/src/main/java/org/cqu/ui/controller/disController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login_register-buyer")
+@RequestMapping("/")
 public class disController {
 
-    @RequestMapping("/login")
+    @RequestMapping("/login_register-buyer/login")
     public String login(String Username, String Password){
         String s = Username+"?"+Password+"?";
         return s;
     }
-    @RequestMapping("/register")
+    @RequestMapping("/login_register-buyer/register")
     public String register(String Username, String Gender, String Address, String Tel, String Password){
+        String s = Username+"?"+Gender+"?"+Address+"?";
+        System.out.println(s);
+        return s;
+    }
+    @RequestMapping("buyer_user_center/update_buyer_info")
+    public String update_buyer_info(String Username, String Gender, String Address, String Tel){
         String s = Username+"?"+Gender+"?"+Address+"?";
         System.out.println(s);
         return s;
