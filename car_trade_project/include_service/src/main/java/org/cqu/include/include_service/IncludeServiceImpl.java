ba26d0af41a7e5a970cd0c46f9e0751abd1c1cc0
SiciliaLee <xslialee070@outlook.com>
2021-07-20 16:49:13 +0800

    复杂查询--cache_service+include_service

---
+++ car_trade_project/include_service/src/main/java/org/cqu/include/include_service/IncludeServiceImpl.java
@@ -0,0 +1,42 @@
+package org.cqu.include.include_service;
+
+import org.apache.dubbo.config.annotation.Service;
+import org.cqu.include_api.IncludeService;
+import org.cqu.mapper.IncludeMapper;
+import org.cqu.pojo.Include;
+import org.cqu.pojo.IncludeExample;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class IncludeServiceImpl implements IncludeService {
+    @Autowired
+    IncludeMapper includeMapper;
+    @Override
+    public List<Integer> findDistinctCID(){
+        IncludeExample ie = new IncludeExample();
+        ie.setDistinct(true);
+        List<Include> res = includeMapper.selectByExample(ie);
+        Set<Integer> cid_set = new HashSet<>();
+        for (Include i: res) {
+            cid_set.add(i.getCid());
+        }
+        List<Integer> cid_distinct = new ArrayList<>(cid_set);
+        return cid_distinct;
+    }
+    @Override
+    public List<Integer> findMCountByCID(Integer cid){
+        IncludeExample ie = new IncludeExample();
+        ie.createCriteria().andCidEqualTo(cid);
+        List<Include> res = includeMapper.selectByExample(ie);
+        List<Integer> mcount_list = new ArrayList<>();
+        for (Include i: res) {
+            mcount_list.add(i.getMcount());
+        }
+        return mcount_list;
+    }
+}
--- car_trade_project/ui/src/main/java/org/cqu/ui/controller/IncludeController.java
+++ car_trade_project/ui/src/main/java/org/cqu/ui/controller/IncludeController.java
@@ -2,7 +2,7 @@ package org.cqu.ui.controller;
 
 
 import org.apache.dubbo.config.annotation.Reference;
-import org.cqu.include.service.IncludeService;
+import org.cqu.include_api.IncludeService;
 import org.cqu.pojo.Include;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +15,12 @@ public class IncludeController {
     @Reference
     private IncludeService includeService;
     @RequestMapping(value = "/findDistinctCID")
-    public List<Include> findDistinctCID() {
+    public List<Integer> findDistinctCID() {
         return includeService.findDistinctCID();
     }
 
     @RequestMapping(value = "/findMCountByCID")
-    public List<Include> findMCountByCID() {return includeService.findMCountByCID(3591);}
+    public List<Integer> findMCountByCID() {
+        return includeService.findMCountByCID(3591);
+    }
 }
