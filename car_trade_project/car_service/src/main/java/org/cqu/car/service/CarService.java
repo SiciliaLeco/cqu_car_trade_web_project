6370969e22974c628da775ee823c2c317a5f1b1f
MinLee621 <15340528305@163.com>
2021-07-22 15:10:45 +0800

    新增CarService相关功能两项 1、根据商家名称获得其所有车辆（已通过测试） 2、商家上传新车辆（未测试）

--- car_trade_project/car_service/src/main/java/org/cqu/car/service/CarService.java
+++ car_trade_project/car_service/src/main/java/org/cqu/car/service/CarService.java
@@ -1,6 +1,5 @@
 package org.cqu.car.service;
 
-import org.cqu.dto.ResultInfo;
 import org.cqu.pojo.Car;
 
 import java.util.List;
@@ -10,4 +9,8 @@ public interface CarService {
     String setCarName(int CarID, String Cname);
     Car getCarInfo(int CarID);
     List<Car> getRandomCar();
+    List<Car> getCarsByType(String CType);
+//    int uploadNewCar(String CName, float CPrice, int CSpeed, String CFuel, String CType,
+//                     String CGear, int Cwarranty, int Ctank_capacity, float CAcceleration,
+//                     String CPic1, String CPic2, String CPic3, String CPic4, String CPic5);
 }
--- car_trade_project/car_service/src/main/java/org/cqu/car/serviceImpl/CarServiceImpl.java
+++ car_trade_project/car_service/src/main/java/org/cqu/car/serviceImpl/CarServiceImpl.java
@@ -4,11 +4,15 @@ import org.apache.dubbo.config.annotation.Service;
 import org.cqu.mapper.CarMapper;
 import org.cqu.pojo.Car;
 import org.cqu.car.service.CarService;
+import org.cqu.pojo.CarExample;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.List;
+
+import static java.lang.Integer.max;
 import static java.lang.Integer.valueOf;
 
 @Service
@@ -46,7 +50,9 @@ public class CarServiceImpl implements CarService {
     public List<Car> getRandomCar(){
         String nginx_info = "http://116.63.170.243:8888/";
         List<Integer> carid_list = new ArrayList<>();
-        carid_list.add(3298);carid_list.add(3481);carid_list.add(4232);carid_list.add(4820);carid_list.add(5810);carid_list.add(5884);carid_list.add(6048);carid_list.add(564);carid_list.add(3121);
+        carid_list.add(3298);carid_list.add(3481);carid_list.add(4232);
+        carid_list.add(4820);carid_list.add(5810);carid_list.add(5884);
+        carid_list.add(6048);carid_list.add(564);carid_list.add(3121);
         List<Car> car_list = new ArrayList<>();
         for(Integer carid: carid_list){
             Car cur_car = carMapper.selectByPrimaryKey(carid);
@@ -59,8 +65,44 @@ public class CarServiceImpl implements CarService {
             car_list.add(cur_car);
         }
         return car_list;
-
-
     }
+
+    @Override
+    public List<Car> getCarsByType(String CType) {
+        CarExample carExample = new CarExample();
+        carExample.createCriteria().andCtypeEqualTo(CType);
+        return carMapper.selectByExample(carExample);
+    }
+
+//    @Override
+//    public int uploadNewCar(String CName, float CPrice, int CSpeed, String CFuel, String CType,
+//                            String CGear, int Cwarranty, int Ctank_capacity, float CAcceleration,
+//                            String CPic1, String CPic2, String CPic3, String CPic4, String CPic5) {
+//        Car newCar = new Car();
+//        newCar.setCname(CName);
+//        newCar.setCprice(CPrice);
+//        newCar.setCspeed(CSpeed);
+//        newCar.setCfuel(CFuel);
+//        newCar.setCtype(CType);
+//        newCar.setCgear(CGear);
+//        newCar.setCwarranty(Cwarranty);
+//        newCar.setCtankCapacity(Ctank_capacity);
+//        newCar.setCacceleration(CAcceleration);
+//        newCar.setCpic1(CPic1);
+//        newCar.setCpic2(CPic2);
+//        newCar.setCpic3(CPic3);
+//        newCar.setCpic4(CPic4);
+//        newCar.setCpic5(CPic5);
+//
+//        CarExample carExample = new CarExample();
+//        List<Car> allCars = carMapper.selectByExample(carExample);
+//        int maxCID = 0;
+//        for (Car car: allCars) {
+//            maxCID = max(maxCID, car.getCid());
+//        }
+//        newCar.setCid(maxCID+1);
+//
+//        return carMapper.insert(newCar);
+//    }
 
 }
