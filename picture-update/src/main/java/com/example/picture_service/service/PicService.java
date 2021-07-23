344c2f12643bee0fd263904a68b7bd5777e9c64c
Sicilia Lee <55285194+SiciliaLeco@users.noreply.github.com>
2021-07-23 22:11:08 +0800

    Delete picture-update directory

--- picture-update/src/main/java/com/example/picture_service/service/PicService.java
+++
@@ -1,7 +0,0 @@
-package com.example.picture_service.service;
-
-import java.io.IOException;
-
-public interface PicService {
-    String upload(byte[] bytes, long fileSize, String extension) throws IOException;
-}
